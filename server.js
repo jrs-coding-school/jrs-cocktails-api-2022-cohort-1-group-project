f5db6da52c6e21b1031053edb0704e1b514aaf3e
kglozoya <101759059+kglozoya@users.noreply.github.com>
2022-05-19 13:13:19 -0400

    updated server and controller files

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+// require('dotenv').config();  is this line necessary when using uuid? and or bcrypt
+
 
 const app = express();
 
@@ -13,7 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 require('./app/index');
-require('./app/routes/palettes.routes.js')(app);
+require('./app/routes/users.routes')(app);
 
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
