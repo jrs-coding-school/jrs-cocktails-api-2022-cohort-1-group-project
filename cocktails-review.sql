5ad2dd1a2b19144cb1168afcbf17f2b638c536fc
bferri42 <79776968+bferri42@users.noreply.github.com>
2022-05-19 15:11:29 -0400

    wrote reviews

---
+++ cocktails-review.sql
@@ -0,0 +1,22 @@
+INSERT INTO `cocktails`.`review` 
+  (`userId`, `drinkId`, `rating` ,`comment`) 
+VALUES 
+  ('7801ff44-d79f-11ec-856c-6b8b7bc362a1', '178306', '4', 'This drink can be a shot or a cocktail. It is delicious.'),
+  ('78021592-d79f-11ec-856c-6b8b7bc362a1', '17228', '2', 'Tasted like toilet water. Wouldnt recommend.'),
+  ('7802168c-d79f-11ec-856c-6b8b7bc362a1', '12754', '5', 'Best recipe Ive found yet.'),
+  ('7802170e-d79f-11ec-856c-6b8b7bc362a1', '11013', '4', 'Made it for my sons graduation, his friends loved it.'),
+  ('78021790-d79f-11ec-856c-6b8b7bc362a1', '11403', '1', 'If I couldve given this less than 1 star, I would have. YUCK!!'),
+  ('78021808-d79f-11ec-856c-6b8b7bc362a1', '12402', '4', 'Perfect for the golf course.'),
+  ('7802186c-d79f-11ec-856c-6b8b7bc362a1', '11728', '3', 'Im no Carrie Bradshaw but this martini was just okay.'),
+  ('780218da-d79f-11ec-856c-6b8b7bc362a1', '11084', '2', 'Reminded me of my honeymoon, were divorced now.'),
+  ('78021948-d79f-11ec-856c-6b8b7bc362a1', '14956', '5', 'SOOOO GOOD!!! Wish I could give this 6 stars.'),
+  ('780219d4-d79f-11ec-856c-6b8b7bc362a1', '11113', '3', 'It was okay. Suggest adding a chicken wing as garnish or some type of food.'),
+  ('7802186c-d79f-11ec-856c-6b8b7bc362a1', '14167', '4', 'Its pretty darn good.'),
+  ('780218da-d79f-11ec-856c-6b8b7bc362a1', '11013', '2', 'Not all that good. Would not recommend'),
+  ('780218da-d79f-11ec-856c-6b8b7bc362a1', '178364', '4', 'MMM Cant go wrong with this classic.'),
+  ('780218da-d79f-11ec-856c-6b8b7bc362a1', '14978', '2', 'Not as good as the ones on the beach.'),
+  ('78021948-d79f-11ec-856c-6b8b7bc362a1', '17248', '5', 'Oui oui, Delicious!'),
+  ('78021948-d79f-11ec-856c-6b8b7bc362a1', '11728', '2', 'Roger Moore would roll over in his grave. This is a 002 not a 007'),
+  ('780219d4-d79f-11ec-856c-6b8b7bc362a1', '17212', '5', 'Spectacular!');
+  ('780219d4-d79f-11ec-856c-6b8b7bc362a1', '11728', '4', 'This martini is perfectly dry for sipping or gulping.');
+  ('780219d4-d79f-11ec-856c-6b8b7bc362a1', '14956', '4', 'These were so good. Lemon jello was the way to go.');
