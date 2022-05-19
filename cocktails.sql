bccbf0c180fa6121fbfc3058d51abf50b3cfe2ed
kglozoya <101759059+kglozoya@users.noreply.github.com>
2022-05-19 15:14:05 -0400

    comma changes to cocktails & cocktails-favorte

--- cocktails.sql
+++ cocktails.sql
@@ -43,8 +43,8 @@ VALUES
   ('7802168c-d79f-11ec-856c-6b8b7bc362a1', 'user3@email.com', '$2b$10$d0yM6cCrMSO6tff3toxfFurhzFxI7wS6G1kcMA8Od3jGmi.LWHtba'),
   ('7802170e-d79f-11ec-856c-6b8b7bc362a1', 'user4@email.com', '$2b$10$SeMGQhm.gsfvqkk6.pJA5ukTHHTaJZPMKnT6AwMp6mjSKNob.Jxim'),
   ('78021790-d79f-11ec-856c-6b8b7bc362a1', 'user5@email.com', '$2b$10$oq2vFyknns8ADkzXN1T75uBQco7fnyh7P5thaBc0FZXdq2Dj9J5Ga'),
-  ('78021808-d79f-11ec-856c-6b8b7bc362a1', 'user6@email.com', '$2b$10$naF6Cg99jZIIqCBECE6yueA7QbCLXkn1FgthPbz/Z0.rHrE/qsTwe');
-  ('7802186c-d79f-11ec-856c-6b8b7bc362a1', 'user7@email.com', '$2b$10$/yWXvXW1YOq1tfHAcxN7aeoe1mhjekbBdYqcgrbVPaQ24Gs8lyJcq');
-  ('780218da-d79f-11ec-856c-6b8b7bc362a1', 'user8@email.com', '$2b$10$K8zpzjVU.BLCkLpcNGZHLux65IqBmSbR87ZG4mvlJ6xLtSG9rbOs6');
-  ('78021948-d79f-11ec-856c-6b8b7bc362a1', 'user9@email.com', '$2b$10$rWVg6//YHif2.aUMlfdd..zpwqulFPl1aLA1nTde2l2iIvs7LSOka');
+  ('78021808-d79f-11ec-856c-6b8b7bc362a1', 'user6@email.com', '$2b$10$naF6Cg99jZIIqCBECE6yueA7QbCLXkn1FgthPbz/Z0.rHrE/qsTwe'),
+  ('7802186c-d79f-11ec-856c-6b8b7bc362a1', 'user7@email.com', '$2b$10$/yWXvXW1YOq1tfHAcxN7aeoe1mhjekbBdYqcgrbVPaQ24Gs8lyJcq'),
+  ('780218da-d79f-11ec-856c-6b8b7bc362a1', 'user8@email.com', '$2b$10$K8zpzjVU.BLCkLpcNGZHLux65IqBmSbR87ZG4mvlJ6xLtSG9rbOs6'),
+  ('78021948-d79f-11ec-856c-6b8b7bc362a1', 'user9@email.com', '$2b$10$rWVg6//YHif2.aUMlfdd..zpwqulFPl1aLA1nTde2l2iIvs7LSOka'),
   ('780219d4-d79f-11ec-856c-6b8b7bc362a1', 'user10@email.com', '$2b$10$QvBgK0fkXSnmDCLhltPnfuUZMabPmgV9kgQyHlW1JzU9UVSEJraJ2');
